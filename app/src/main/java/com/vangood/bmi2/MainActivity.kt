86df4e38202c2a0b0455a9a3417d62e432343c65
lm <f10408034@gmail.com>
2022-03-03 16:38:49 +0800

    second commit add height weight

--- app/src/main/java/com/vangood/bmi2/MainActivity.kt
+++ app/src/main/java/com/vangood/bmi2/MainActivity.kt
@@ -2,10 +2,22 @@ package com.vangood.bmi2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import com.vangood.bmi2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    fun bmi(view: View){
+        println("hahaha")
+        var weight = binding.edWeight.text.toString().toFloat()
+        var height = binding.edHeight.text.toString().toFloat()
+        println(weight/(height*height))
     }
 }
